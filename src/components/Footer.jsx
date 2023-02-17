a40095f6a74278c54e35fabffe2566db58a90a52
bengin34 <becaglar3434@gmail.com>
2023-02-17 12:19:41 +0300

    done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,11 @@ const Footer = () => {
   return (
     <div className="bg-gray-100 text-center text-white">
       <div className="text-center text-gray-700 p-4">
-        © 2021 Copyright:
-        <a className="text-gray-800" href="https://tailwind-elements.com/">
-          Tailwind Elements
+        <a
+          className="text-gray-800"
+          href="https://www.viator.com/support/termsAndConditions"
+        >
+          © 1997–2023 Viator, Inc.
         </a>
       </div>
     </div>
